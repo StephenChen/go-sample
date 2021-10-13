f6e14ce510cbe9361562acabc0866cd7ea754bd9
StephenChen <stephenchen1994@outlook.com>
2021-10-13 19:03:11 +0800

    UPDATE: update for cgo.

--- cgo/hello.go
+++ cgo/hello.go
@@ -1,10 +1,12 @@
+// hello.go
 package main
 
+//#include <stdio.h>
+import "C"
 import "cgo/pkg"
 
-//import "C"
-
 func main() {
+	C.puts(C.CString("hello c world\n"))
 	println("hello cgo")
 	println(pkg.Id)
 	println(pkg.Name)
