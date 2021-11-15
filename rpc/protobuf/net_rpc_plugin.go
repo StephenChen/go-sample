ee23e71dabcf08179ae4644cd0d4707035c94907
StephenChen <stephenchen1994@outlook.com>
2021-11-15 23:41:03 +0800

    UPDATE: update rpc.

--- rpc/protobuf/net_rpc_plugin.go
+++ rpc/protobuf/net_rpc_plugin.go
@@ -1,6 +1,9 @@
 package main
 
-import "github.com/golang/protobuf/protoc-gen-go/generator"
+import (
+	"github.com/golang/protobuf/protoc-gen-go/descriptor"
+	"github.com/golang/protobuf/protoc-gen-go/generator"
+)
 
 type netRpcPlugin struct {
 	*generator.Generator
@@ -15,13 +18,21 @@ func (p *netRpcPlugin) Init(g *generator.Generator) {
 }
 
 func (p *netRpcPlugin) Generate(file *generator.FileDescriptor) {
-	//for _, svc := range file.Service {
-	//	p.genServiceCode(svc)
-	//}
+	for _, svc := range file.Service {
+		p.genServiceCode(svc)
+	}
 }
 
 func (p *netRpcPlugin) GenerateImports(file *generator.FileDescriptor) {
 	if len(file.Service) > 0 {
-		//p.genImportCode(file)
+		p.genImportCode(file)
 	}
+}
+
+func (p *netRpcPlugin) genImportCode(file *generator.FileDescriptor) {
+	p.P("// TODO: import code")
+}
+
+func (p *netRpcPlugin) genServiceCode(svc *descriptor.ServiceDescriptorProto) {
+	p.P("// TODO: service code, Name = " + svc.GetName())
 }
